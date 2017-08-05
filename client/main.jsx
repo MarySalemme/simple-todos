6ba09545c950c8f9c15ed12e0e9a75b54bd08573
MarySalemme <marysalemme1@gmail.com>
2017-08-05 20:43:06 +0100

    Modifying html

--- client/main.jsx
+++ client/main.jsx
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 
 import App from '../imports/ui/App.jsx';
 
+
+// This code loads the App and Task components and renders them into the #render-target html element.
 Meteor.startup(() =>{
   render(<App />, document.getElementById('render-target'));
 });
